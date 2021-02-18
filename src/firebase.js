6ac6fbc07e29268b5abe4b9c35ba7a0e6b2ceda5
Владислав Касинцев <Iamkasvlad@gmail.com>
2021-02-18 10:25:21 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,8 @@
 import firebase from "firebase"
 
 const firebaseCfg = {
-    apiKey: "AIzaSyCLNXmFsfp6Uu5CPPZXf42j07RPTek2FT0",
-    authDomain: "chatapp-f52f4.firebaseapp.com",
-    projectId: "chatapp-f52f4",
-    storageBucket: "chatapp-f52f4.appspot.com",
-    messagingSenderId: "296118407503",
-    appId: "1:296118407503:web:cbed5a10fcaa6a5df2753a"
+    //add your config
+   
 };
 const firebaseApp = firebase.initializeApp(firebaseCfg);
 const db = firebaseApp.firestore();
